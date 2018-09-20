a62c673df821f5b4f050dd8ef23a831ae06e7238
Talhashahar <talhashahar@gmail.com>
2018-09-20 14:03:08 +0300

    Add files via upload

--- docker-test.py
+++ docker-test.py
@@ -1,1 +1,25 @@
-print 'Check Docker from github'
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello Avi, Docker name is{name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits} <br/><br/><br/><br/>"\
+		   "<h1>Tal The King is Rulez</h1>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
